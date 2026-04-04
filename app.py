5a53688c31bae2a68873bc69f02a3f1abbd53cd5
Rishabh Patel <rishabhpatel92786gmail.com>
2026-04-04 18:23:18 +0530

    Add Procfile and gunicorn for deployment

--- app.py
+++ app.py
@@ -25,6 +25,30 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text=f'Estimated Price: ${output}')
+import matplotlib.pyplot as plt
+import io
+import base64
 
+@app.route('/visualize')
+def visualize():
+    # Simple data for the graph
+    sqft = [1000, 1500, 2000, 2500, 3000, 3500]
+    price = [100000, 150000, 200000, 230000, 300000, 350000]
+    
+    plt.figure(figsize=(6,4))
+    plt.scatter(sqft, price, color='blue', label='Actual Data')
+    plt.plot(sqft, price, color='red', label='Regression Line')
+    plt.xlabel('Square Feet')
+    plt.ylabel('Price ($)')
+    plt.title('House Price Trend')
+    plt.legend()
+    
+    # Save plot to a fake file in memory
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    
+    return render_template('index.html', plot_url=plot_url)
 if __name__ == "__main__":
     app.run(debug=True)
